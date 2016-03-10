b81646f39f2c6423c76efa99f3b62d83c51d66dc
小林 進治 <classicmasterpeace@shourin-no-MacBook-Air.local>
2016-03-11 00:27:57 +0900

    file updated

--- js/carr-mobile.js
+++ js/carr-mobile.js
@@ -1,14 +1,44 @@
 $(function() {
-  var $carrContents = $('.js-carr-contents');
+var x,y;
 
-  // タッチイベントに対応しているかを判定
-  var isTouch = 'touchend' in document;
+// タッチイベントに対応しているかを判定
+var isTouch = 'ontouchend' in document;
 
-  $carrContents.each(function() {
-    $this = $(this);
+// js-test
+if(isTouch) {
+  $(document).on({
+    'touchstart' : touchStart,
+    'touchmove' : touchMove,
+    'touchend' : touchEnd
+  },'.js-test-hitarea');
+}
 
-    $this.on('touchend',function(e) {
-      console.log(e.originalEvent);
-    })
-  });
+function touchStart(e) {
+  var original = e.originalEvent;
+
+  x = original.changedTouches[0].pageX;
+  y = original.changedTouches[0].pageY;
+
+  $('.startx').text('startX : ' + x);
+  $('.starty').text('startY : ' + y);
+};
+
+function touchMove(e){
+  var original = e.originalEvent;
+  e.preventDefault();
+
+  x = original.changedTouches[0].pageX;
+  y = original.changedTouches[0].pageY;
+
+  $('.movex').text('moveX : ' + x);
+  $('.movey').text('moveY : ' + y);
+}
+
+function touchEnd() {
+  $('.endx').text('endX : ' + x);
+  $('.endy').text('endY : ' + y);
+}
 });
+
+
+// js-carr
